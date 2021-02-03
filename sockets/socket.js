a83aacb6945c24d20bca72a1feac1a9e5a37ace2
breinersn <bsuarezn1@unicartagena.edu.co>
2021-02-02 19:26:53 -0500

    make it better

--- sockets/socket.js
+++ sockets/socket.js
@@ -1,20 +1,29 @@
 const { io } = require('../index');
+const Band = require('../models/band');
+const Bands = require('../models/bands');
 
+const bands = new Bands();
+
+bands.addBand(new Band('Los pericos'));
+bands.addBand(new Band('Los cafres'));
+bands.addBand(new Band('Gonwana'));
+bands.addBand(new Band('Zona ganja'));
 
 // Mensajes de Sockets
 io.on('connection', client => {
     console.log('Cliente conectado');
 
+    client.emit('bandas-activas', bands.getBands()); //emitir todas las bandas
+
     client.on('disconnect', () => {
         console.log('Cliente desconectado');
     });
 
-    client.on('mensaje', (payload) => {
-        console.log('Mensaje', payload);
+    // client.on('mensaje', (payload) => {
+    //     console.log('Mensaje', payload);
 
-        io.emit('mensaje', { admin: 'Nuevo mensaje' });
+    //     io.emit('mensaje', { admin: 'Nuevo mensaje' });
 
-    });
-
+    // });
 
 });
