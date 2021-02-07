10719971c31c1903df245c5175bc5ee29bcffc19
breinersn <bsuarezn1@unicartagena.edu.co>
2021-02-06 21:36:24 -0500

    crud listo

--- sockets/socket.js
+++ sockets/socket.js
@@ -8,22 +8,32 @@ bands.addBand(new Band('Los pericos'));
 bands.addBand(new Band('Los cafres'));
 bands.addBand(new Band('Gonwana'));
 bands.addBand(new Band('Zona ganja'));
+bands.addBand(new Band('cultura profetica'));
 
 // Mensajes de Sockets
 io.on('connection', client => {
     console.log('Cliente conectado');
 
-    client.emit('bandas-activas', bands.getBands()); //emitir todas las bandas
+    client.emit('bandas-activas', bands.getBands()); //emitir todas las bandas al cliente que se conecte
 
     client.on('disconnect', () => {
         console.log('Cliente desconectado');
     });
 
-    // client.on('mensaje', (payload) => {
-    //     console.log('Mensaje', payload);
+    client.on('band-votes', (payload) => {   
+        bands.voteBand(payload.id); //hacer la votacion
+        io.emit('bandas-activas', bands.getBands()); //emitir las bandas a todos los clientes
+    });
 
-    //     io.emit('mensaje', { admin: 'Nuevo mensaje' });
+    client.on('band-name', ( payload) => {
+        const newBand = new Band( payload.name );
+        bands.addBand(newBand);
+        io.emit('bandas-activas', bands.getBands()); //emitir las bandas a todos los clientes
+    });
 
-    // });
+    client.on('band-delete', (payload) => {
+        bands.deleteBand(payload.id);
+        io.emit('bandas-activas', bands.getBands()); //emitir las bandas a todos los clientes
+    });
 
 });
